ab9aedb0622cdc855b40a2d164f566a44867d41c
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2021-04-29 19:22:37 -0300

    Final Commit

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -12,37 +19,46 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day:"numeric",
-    month:"long"
-  };
-
-  var day = today.toLocaleDateString("en-US",options);
-
-
-  res.render("list", {kindOfDay: day, newListItems: items});
+  var day = date.getDate();
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req,res){
 var item = req.body.newItem;
 
-items.push(item);
+if (req.body.list === "Work"){
 
+  workItems.push(item);
 
-res.redirect("/");
+  res.redirect("/work");
+}else{
 
+  items.push(item);
 
-
+  res.redirect("/");
+}
 });
 
 
+app.get("/work", function(req,res){
 
+  res.render("list",{listTitle:"Work List", newListItems: workItems });
 
+});
 
+app.post("/work", function(req,res){
+  var item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+
+});
+
+app.get("/about", function(req,res){
+
+  res.render("about");
+
+});
 
 
 
